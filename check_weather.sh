3de37b2023714637ac64b25b05eb2b10330b7730
Devarsh Dixit <126683540+devdixit007@users.noreply.github.com>
2023-03-09 14:40:26 +0530

    Add files via upload

---
+++ check_weather.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Author : Dev Dixit (www.instagram.com/devdixit.sh)
+
+echo "Please wait script is starting..."
+sleep 4
+echo ____________________________________________________________________________________________________________
+echo "[+] Enter The City Name: "
+read city
+
+for x in $city
+do
+    weather=$(curl -s http://wttr.in/$x?format=3)
+        echo "The weather for $weather"
+
+done
